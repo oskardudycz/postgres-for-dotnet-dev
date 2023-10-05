458e9c3f70d2633d88a37456b817cf9236fb7164
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-10-05 11:49:32 +0200

    Added more options to publication management

--- PostgresForDotnetDev.Api/FleetManagement/FuelEfficiencyAlertsPostgresSubscription.cs
+++ PostgresForDotnetDev.Api/FleetManagement/FuelEfficiencyAlertsPostgresSubscription.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using PostgresOutbox.Subscriptions;
+using PostgresOutbox.Subscriptions.Management;
 using PostgresOutbox.Subscriptions.Replication;
 using static PostgresForDotnetDev.Api.FleetManagement.FleetManagementHub;
 
@@ -14,23 +15,30 @@ public class FuelEfficiencyAlertsPostgresSubscription
     )
     {
         const string slotName = "fuel_efficiency_alerts_slot";
+        var createStyle = CreateStyle.WhenNotExists;
 
         var dataMapper = new FlatObjectMapper<FuelEfficiencyAlert>(NameTransformations.FromPostgres);
 
         var subscriptionOptions = new SubscriptionOptions(
             connectionString,
-            slotName,
-            "fuel_efficiency_alerts_pub",
-            "fuel_efficiency_alerts",
-            dataMapper,
-            CreateStyle.WhenNotExists
+            new PublicationManagement.PublicationSetupOptions(
+                "fuel_efficiency_alerts_pub",
+                "fuel_efficiency_alerts",
+                createStyle,
+                true
+            ),
+            new ReplicationSlotManagement.ReplicationSlotSetupOptions(
+                slotName,
+                createStyle
+            ),
+            dataMapper
         );
 
         var subscription = new Subscription();
 
         await foreach (var alert in subscription.Subscribe(subscriptionOptions, ct: ct))
         {
-           await SendFuelEfficiencyAlert(hubContext, (FuelEfficiencyAlert) alert);
+            await SendFuelEfficiencyAlert(hubContext, (FuelEfficiencyAlert)alert);
         }
     }
 }
